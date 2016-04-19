2010d3e60dc2168b889d765028128a1567fa5d46
Noah Zoschke <nzoschke@gmail.com>
2016-04-18 19:05:58 -0700

    refactor main and split and parse content into syslog message

--- main.go
+++ main.go
@@ -4,7 +4,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"net/url"
 	"os"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
 
 	syslog "github.com/RackSec/srslog"
 	"github.com/aws/aws-sdk-go/aws"
@@ -16,41 +21,19 @@ import (
 
 func main() {
 	lambda_proc.Run(func(context *lambda_proc.Context, eventJSON json.RawMessage) (interface{}, error) {
-		url := ""
-
-		data, err := ioutil.ReadFile("/tmp/url")
-		if err == nil {
-			url = string(data)
-			fmt.Fprintf(os.Stderr, "ioutil.ReadFile url=%s\n", url)
-		} else {
-			fmt.Fprintf(os.Stderr, "ioutil.ReadFile err=%s\n", err)
-
-			cf := cloudformation.New(session.New(&aws.Config{}))
-
-			resp, err := cf.DescribeStacks(&cloudformation.DescribeStacksInput{
-				StackName: aws.String("test-syslog"),
-			})
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "cf.DescribeStacks err=%s\n", err)
-			} else {
-				fmt.Fprintf(os.Stderr, "cf.DescribeStacks resp=%+v\n", resp)
-			}
-
-			if len(resp.Stacks) == 1 {
-				params := resp.Stacks[0].Parameters
-				for _, p := range params {
-					if *p.ParameterKey == "Url" {
-						url = *p.ParameterValue
-
-						ioutil.WriteFile("/tmp/url", []byte(url), 0644)
-						fmt.Fprintf(os.Stderr, "ioutil.WriteFile url=%s\n", url)
-						break
-					}
-				}
-			}
+		syslogUrl, err := readOrDescribeURL(context.FunctionName)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "readOrDescribeURL err=%s\n", err)
+			return nil, err
 		}
 
-		fmt.Fprintf(os.Stderr, "url=%s\n", url)
+		u, err := url.Parse(syslogUrl)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "url.Parse url=%s err=%s\n", syslogUrl, err)
+			return nil, err
+		}
+
+		fmt.Fprintf(os.Stderr, "event=%q url=%s\n", eventJSON, u.String())
 
 		var event cloudwatchlogs.Event
 		err = json.Unmarshal([]byte(eventJSON), &event)
@@ -65,16 +48,20 @@ func main() {
 			return nil, err
 		}
 
-		w, err := syslog.Dial("tcp", url, syslog.LOG_INFO, "convox/syslog")
+		fmt.Fprintf(os.Stderr, "DecodedData=%+v\n", d)
+
+		w, err := syslog.Dial(u.Scheme, u.Host, syslog.LOG_INFO, "convox/syslog")
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "syslog.Dial err=%s\n", err)
+			fmt.Fprintf(os.Stderr, "syslog.Dial scheme=%s host=%s err=%s\n", u.Scheme, u.Host, err)
 			return nil, err
 		}
 		defer w.Close()
 
+		w.SetFormatter(contentFormatter)
+
 		logs, errs := 0, 0
 		for _, e := range d.LogEvents {
-			err := w.Info(e.Message)
+			err := w.Info(fmt.Sprintf("%s %d %s", d.LogGroup, e.Timestamp, e.Message))
 			if err != nil {
 				errs += 1
 			} else {
@@ -84,4 +71,86 @@ func main() {
 
 		return fmt.Sprintf("LogGroup=%s LogStream=%s MessageType=%s NumLogEvents=%d logs=%d errs=%d", d.LogGroup, d.LogStream, d.MessageType, len(d.LogEvents), logs, errs), nil
 	})
+}
+
+var Re = regexp.MustCompile(`^([^:]+):([^/]+)/([^ ]+) (.*)(\n)?$`)
+
+// contentFormatter parses the content string to populate the entire syslog RFC5424 message. No os information is referenced.
+// convox-httpd-LogGroup-1KIJO8SS9F3Q9 1460682044602 web:RGBCKLEZHCX/ec329dcefd61 10.0.3.37 - - [15/Apr/2016:01:00:44 +0000] "GET / HTTP/1.1" 304 -
+func contentFormatter(p syslog.Priority, hostname, tag, content string) string {
+	hostname = os.Getenv("AWS_LAMBDA_FUNCTION_NAME")
+	timestamp := time.Now()
+	program := "convox/syslog"
+	tag = "unknown"
+
+	parts := strings.SplitN(content, " ", 3)
+
+	if len(parts) == 3 {
+		hostname = parts[0]
+
+		i, err := strconv.ParseInt(parts[1], 10, 64)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "stcvonv.ParseInt s=%s err=%s\n", parts[1], err)
+		} else {
+			sec := i / 1000
+			nsec := i - (sec * 1000)
+			timestamp = time.Unix(sec, nsec)
+		}
+
+		content = parts[2]
+	}
+
+	if m := Re.FindStringSubmatch(content); m != nil {
+		program = fmt.Sprintf("%s:%s", m[1], m[2])
+		tag = m[3]
+		content = m[4]
+	} else {
+		fmt.Fprintf(os.Stderr, "Re.FindStringSubmatch miss\n")
+	}
+
+	msg := fmt.Sprintf("<%d>%d %s %s %s %s - - %s\n",
+		22, 1, timestamp.Format(time.RFC3339), hostname, program, tag, content)
+
+	return msg
+}
+
+func readOrDescribeURL(name string) (string, error) {
+	data, err := ioutil.ReadFile("/tmp/url")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ioutil.ReadFile err=%s\n", err)
+	} else {
+		fmt.Fprintf(os.Stderr, "ioutil.ReadFile url=%s\n", string(data))
+		return string(data), nil
+	}
+
+	cf := cloudformation.New(session.New(&aws.Config{}))
+
+	resp, err := cf.DescribeStacks(&cloudformation.DescribeStacksInput{
+		StackName: aws.String(name),
+	})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "cf.DescribeStacks err=%s\n", err)
+		return "", err
+	}
+
+	fmt.Fprintf(os.Stderr, "cf.DescribeStacks resp=%+v\n", resp)
+
+	if len(resp.Stacks) == 1 {
+		for _, p := range resp.Stacks[0].Parameters {
+			if *p.ParameterKey == "Url" {
+				url := *p.ParameterValue
+
+				err := ioutil.WriteFile("/tmp/url", []byte(url), 0644)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "ioutil.WriteFile url=%s err=%s\n", url, err)
+				} else {
+					fmt.Fprintf(os.Stderr, "ioutil.WriteFile url=%s\n", url)
+				}
+
+				return url, nil
+			}
+		}
+	}
+
+	return "", fmt.Errorf("Could not find stack %s Url Parameter", name)
 }
